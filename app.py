5ba0a15133dfb20d049d3077bc8c1b03fb2af42a
Kausthub Kesavadas <kausthub13@gmail.com>
2020-03-09 00:04:27 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask,jsonify,request
+import requests
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route('/base64',methods=['POST'])
+def base64_to_binary():
+    request_data = request.get_json()
+    base64str = request_data['url']
+    url = 'https://api.imgur.com/3/image'
+    myobj = {
+        'image': base64str}
+    headers = {'Content-Type': 'application/x-www-form-urlencoded',
+               'Authorization': 'Client-ID 641970866dba431', }
+    x = requests.post(url, headers=headers, data=myobj)
+    y = json.loads(x.text)
+    return y['data']['link']
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
